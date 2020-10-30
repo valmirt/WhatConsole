375d4dff02e4c62ae61301fa167568d9f491183e
valmirt <valmirt.dev@gmail.com>
2020-10-29 21:55:17 -0300

    Initial Commit

---
+++ WhatConsole/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhatConsole
+//
+//  Created by Valmir Junior on 29/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
